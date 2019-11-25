197eec9fa9c89910d817f38896b0315c85095d0a
Paul-Kyle-Turner <turnerp5@students.rowan.edu>
2019-11-24 22:41:05 -0500

    gather stocks to a single dataframe from all files

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import numpy as np
 import math
 import sqlite3
 import re
-
+from os import listdir
+from os.path import isfile, join
 
 # stock data will remain within a dataframe of col stocks, end of day, end of day increase decrease
 # news information will be gathered from the news.db
@@ -13,6 +14,22 @@ def gather_data():
     print('working')
 
 
+# python array of files by pycruft, stackoverflow
+# this function concats the stock data from different sources within a path directory
+def gather_files_from_dir(path):
+    dataframe_stocks_complete = pd.DataFrame()
+
+    # gathers a list of names of all files in the dir
+    stock_files = [f for f in listdir(path) if isfile(join(path, f))]
+
+    for file in stock_files:
+        temp_stocks_dataframe = pd.read_pickle(path + '/' + file)
+        dataframe_stocks_complete = dataframe_stocks_complete.append(temp_stocks_dataframe)
+
+    return dataframe_stocks_complete
+
+
+
 def gather_data_from_stocks():
     # Alex please make this function
     # It should take in a pandas dataframe of the stock information and return a dataframe that includes the stock
@@ -20,7 +37,7 @@ def gather_data_from_stocks():
     print('ALEX')
 
 
-# gather data from the sqlite3 database into a list for easier useage
+# gather data from the sqlite3 database into a list for easier usage
 def gather_news_content(database_path):
     connect = sqlite3.connect(database_path)
     cursor = connect.cursor()
@@ -34,6 +51,10 @@ def gather_news_content(database_path):
 
     # replacer will take away the special chars, and end of document [+number chars]
     news_data = replacer(news_data)
+
+    # close connection and cursor
+    cursor.close()
+    connect.close()
 
     return news_data
 
@@ -57,14 +78,19 @@ def replacer(list_data):
         temp_last = ''
         for index in range(len(content) - 1):
             if content[index] is not None:
+
+                # replaces certain parts of the content of the data for better readability
                 content[index] = re.sub(r'\s+', ' ', content[index])
                 content[index] = re.sub(r'<[/]*\w>', '', content[index])
                 end_string_list = content[index].split('[')
+
+                # checks to see if the description is the same as the content of the file.
                 if index == 1 and index != 0:
                     temp_last = content[index]
                 else:
                     if temp_last != end_string_list[0]:
                         temp_collect.append(end_string_list[0])
+
         content = [query, ' '.join(temp_collect), date]
         total_list.append(content)
     return total_list
@@ -76,4 +102,7 @@ if __name__ == '__main__':
     # There exist data which the query is None, this data was collected with the use of an old version of searchthenews
     # This can be used for another Y_test set for determining which class of news it was pulled from
     news_data = gather_news_content('news.db')
+
+
+
 
